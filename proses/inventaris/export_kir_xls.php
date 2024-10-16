9abb5c49eb0c293fe2807c0720119e2fc1e3ec88
rinarin-o-o <138567129+rinarin-o-o@users.noreply.github.com>
2024-10-16 08:46:05 +0700

    sarin

--- proses/inventaris/export_kir_xls.php
+++ proses/inventaris/export_kir_xls.php
@@ -19,47 +19,48 @@ $rowlokasi = mysqli_fetch_assoc($result_lokasi);
 
 // Buat objek Spreadsheet
 $spreadsheet = new Spreadsheet();
-$sheet = $spreadsheet->getActiveSheet();
+$sheet1 = $spreadsheet->getActiveSheet();
+$sheet1->setTitle('KIR TABEL');
 $spreadsheet->getDefaultStyle()->getFont()->setName('Times New Roman');
 
 // **Header dan Logo**
-$sheet->setCellValue('C1', 'PEMERINTAH KABUPATEN BREBES');
-$sheet->setCellValue('C2', 'DINAS KOMUNIKASI INFORMATIKA DAN STATISTIK');
-$sheet->setCellValue('C3', 'KARTU INVENTARIS RUANGAN');
-$sheet->mergeCells('C1:J1');
-$sheet->mergeCells('C2:J2');
-$sheet->mergeCells('C3:J3');
-$sheet->mergeCells('A1:B3');
-$sheet->getStyle('C1:C4')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-$sheet->getStyle('C1:C4')->getFont()->setBold(true);
+$sheet1->setCellValue('C1', 'PEMERINTAH KABUPATEN BREBES');
+$sheet1->setCellValue('C2', 'DINAS KOMUNIKASI INFORMATIKA DAN STATISTIK');
+$sheet1->setCellValue('C3', 'KARTU INVENTARIS RUANGAN');
+$sheet1->mergeCells('C1:J1');
+$sheet1->mergeCells('C2:J2');
+$sheet1->mergeCells('C3:J3');
+$sheet1->mergeCells('A1:B3');
+$sheet1->getStyle('C1:C4')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+$sheet1->getStyle('C1:C4')->getFont()->setBold(true);
 $drawing = new Drawing();
 $drawing->setName('Logo');
-$drawing->setPath('brebes.png');
+$drawing->setPath('qrcodes/brebes.png');
 $drawing->setWidth(60);
 $drawing->setCoordinates('A1');
-$drawing->setWorksheet($sheet);
+$drawing->setWorksheet($sheet1);
 
-$sheet->setCellValue('A5', 'Sub Unit Organisasi');
-$sheet->setCellValue('A6', 'Pengelola Barang');
-$sheet->setCellValue('A7', isset($rowlokasi['bid_lokasi']) ? 'Ruangan' : 'Ruangan');
-$sheet->setCellValue('A8', 'Per Tanggal');
-$sheet->setCellValue('C5', ': DINAS KOMUNIKASI, INFORMATIKA DAN STATISTIK');
-$sheet->setCellValue('C6', ': DINAS KOMUNIKASI, INFORMATIKA DAN STATISTIK');
-$sheet->setCellValue('C7', ': ' . strtoupper($rowlokasi['nama_lokasi']));
-$sheet->setCellValue('C8', ': ' . date('d F Y'));
-$sheet->setCellValue('I8', 'No. Kode Lokasi');
-$sheet->setCellValue('K8', ': ' . $rowlokasi['id_lokasi']);
+$sheet1->setCellValue('A5', 'Sub Unit Organisasi');
+$sheet1->setCellValue('A6', 'Pengelola Barang');
+$sheet1->setCellValue('A7', isset($rowlokasi['bid_lokasi']) ? 'Ruangan' : 'Ruangan');
+$sheet1->setCellValue('A8', 'Per Tanggal');
+$sheet1->setCellValue('C5', ': DINAS KOMUNIKASI, INFORMATIKA DAN STATISTIK');
+$sheet1->setCellValue('C6', ': DINAS KOMUNIKASI, INFORMATIKA DAN STATISTIK');
+$sheet1->setCellValue('C7', ': ' . strtoupper($rowlokasi['nama_lokasi']));
+$sheet1->setCellValue('C8', ': ' . date('d F Y'));
+$sheet1->setCellValue('I8', 'No. Kode Lokasi');
+$sheet1->setCellValue('K8', ': ' . $rowlokasi['id_lokasi']);
 
-$sheet->mergeCells('A5:B5');
-$sheet->mergeCells('A6:B6');
-$sheet->mergeCells('A7:B7');
-$sheet->mergeCells('A8:B8');
-$sheet->mergeCells('I8:J8');
-$sheet->mergeCells('K8:M8');
-$sheet->mergeCells('C5:G5');
-$sheet->mergeCells('C6:G6');
-$sheet->mergeCells('C7:G7');
-$sheet->mergeCells('C8:G8');
+$sheet1->mergeCells('A5:B5');
+$sheet1->mergeCells('A6:B6');
+$sheet1->mergeCells('A7:B7');
+$sheet1->mergeCells('A8:B8');
+$sheet1->mergeCells('I8:J8');
+$sheet1->mergeCells('K8:M8');
+$sheet1->mergeCells('C5:G5');
+$sheet1->mergeCells('C6:G6');
+$sheet1->mergeCells('C7:G7');
+$sheet1->mergeCells('C8:G8');
 
 // **Header Kolom Tabel**
 $headers = [
@@ -67,31 +68,31 @@ $headers = [
     'Ukuran', 'Bahan', 'Tahun Pembelian', 'No. Kode Barang', 
     'Jumlah Barang', 'Harga Beli', 'Kondisi Barang'
 ];
-$sheet->fromArray($headers, NULL, 'A10');
+$sheet1->fromArray($headers, NULL, 'A10');
 
 $headers_num = [
     '1', '2', '3', '4', '5', '6','7', '8', '9','10', '11', '12','13'
 ];
-$sheet->fromArray($headers_num, NULL, 'A12'); 
+$sheet1->fromArray($headers_num, NULL, 'A12'); 
 
-$sheet->mergeCells('K10:M10');
-$sheet->mergeCells('A10:A11');
-$sheet->mergeCells('B10:B11');
-$sheet->mergeCells('C10:C11');
-$sheet->mergeCells('D10:D11');
-$sheet->mergeCells('E10:E11');
-$sheet->mergeCells('F10:F11');
-$sheet->mergeCells('G10:G11');
-$sheet->mergeCells('H10:H11');
-$sheet->mergeCells('I10:I11');
-$sheet->mergeCells('J10:J11');
-$sheet->mergeCells('K10:K11');
-$sheet->mergeCells('L10:L11');
-$sheet->mergeCells('M10:M11');
+$sheet1->mergeCells('K10:M10');
+$sheet1->mergeCells('A10:A11');
+$sheet1->mergeCells('B10:B11');
+$sheet1->mergeCells('C10:C11');
+$sheet1->mergeCells('D10:D11');
+$sheet1->mergeCells('E10:E11');
+$sheet1->mergeCells('F10:F11');
+$sheet1->mergeCells('G10:G11');
+$sheet1->mergeCells('H10:H11');
+$sheet1->mergeCells('I10:I11');
+$sheet1->mergeCells('J10:J11');
+$sheet1->mergeCells('K10:K11');
+$sheet1->mergeCells('L10:L11');
+$sheet1->mergeCells('M10:M11');
 
-$sheet->setCellValue('K11', 'Baik (B)');
-$sheet->setCellValue('L11', 'Kurang Baik (KB)');
-$sheet->setCellValue('M11', 'Rusak Berat (RB)');
+$sheet1->setCellValue('K11', 'Baik (B)');
+$sheet1->setCellValue('L11', 'Kurang Baik (KB)');
+$sheet1->setCellValue('M11', 'Rusak Berat (RB)');
 
 // Style Header
 $headerStyle = [
@@ -100,7 +101,7 @@ $headerStyle = [
     'borders' => ['allBorders' => ['borderStyle' => Border::BORDER_THIN]],
     'fill' => ['fillType' => Fill::FILL_SOLID, 'startColor' => ['rgb' => 'D9E1F2']]
 ];
-$sheet->getStyle('A10:M12')->applyFromArray($headerStyle);
+$sheet1->getStyle('A10:M12')->applyFromArray($headerStyle);
 
 // **Isi Data Barang**
 $rowNum = 13;
@@ -131,49 +132,49 @@ $no = 1; // Nomor urut
 
 // Loop untuk mengisi data barang
 while ($row = mysqli_fetch_assoc($result_barang)) {
-    $sheet->setCellValue('A' . $rowNum, $no);
-    $sheet->setCellValue('B' . $rowNum, $row['nama_barang']);
-    $sheet->setCellValue('C' . $rowNum, $row['merk']);
-    $sheet->setCellValue('D' . $rowNum, $row['no_pabrik']);
-    $sheet->setCellValue('E' . $rowNum, $row['ukuran_CC']);
-    $sheet->setCellValue('F' . $rowNum, $row['bahan']);
-    $sheet->setCellValue('G' . $rowNum, $row['tahun_pembelian']);
-    $sheet->setCellValue('H' . $rowNum, $row['kode_barang']);
-    $sheet->setCellValue('I' . $rowNum, $row['total_register']);
-    $sheet->setCellValue('J' . $rowNum, $row['total_harga_awal']);
-    $sheet->setCellValue('K' . $rowNum, $row['jml_baik']);
-    $sheet->setCellValue('L' . $rowNum, $row['jml_kurang_baik']);
-    $sheet->setCellValue('M' . $rowNum, $row['jml_rusak']);
+    $sheet1->setCellValue('A' . $rowNum, $no);
+    $sheet1->setCellValue('B' . $rowNum, $row['nama_barang']);
+    $sheet1->setCellValue('C' . $rowNum, $row['merk']);
+    $sheet1->setCellValue('D' . $rowNum, $row['no_pabrik']);
+    $sheet1->setCellValue('E' . $rowNum, $row['ukuran_CC']);
+    $sheet1->setCellValue('F' . $rowNum, $row['bahan']);
+    $sheet1->setCellValue('G' . $rowNum, $row['tahun_pembelian']);
+    $sheet1->setCellValue('H' . $rowNum, $row['kode_barang']);
+    $sheet1->setCellValue('I' . $rowNum, $row['total_register']);
+    $sheet1->setCellValue('J' . $rowNum, $row['total_harga_awal']);
+    $sheet1->setCellValue('K' . $rowNum, $row['jml_baik']);
+    $sheet1->setCellValue('L' . $rowNum, $row['jml_kurang_baik']);
+    $sheet1->setCellValue('M' . $rowNum, $row['jml_rusak']);
     $rowNum++;
     $no++;
 }
 
 // Baris untuk menampilkan jumlah total
 $totalRow = $rowNum; // Baris setelah data terakhir
-$sheet->setCellValue('A' . $totalRow, 'Jumlah');
-$sheet->mergeCells('A' . $totalRow . ':H' . $totalRow); // Menggabungkan kolom A hingga H untuk label "Jumlah"
+$sheet1->setCellValue('A' . $totalRow, 'Jumlah');
+$sheet1->mergeCells('A' . $totalRow . ':H' . $totalRow); // Menggabungkan kolom A hingga H untuk label "Jumlah"
 
 // Mengisi total dengan formula SUM()
-$sheet->setCellValue('I' . $totalRow, '=SUM(I13:I' . ($totalRow - 1) . ')');
-$sheet->setCellValue('J' . $totalRow, '=SUM(J13:J' . ($totalRow - 1) . ')');
-$sheet->setCellValue('K' . $totalRow, '=SUM(K13:K' . ($totalRow - 1) . ')');
-$sheet->setCellValue('L' . $totalRow, '=SUM(L13:L' . ($totalRow - 1) . ')');
-$sheet->setCellValue('M' . $totalRow, '=SUM(M13:M' . ($totalRow - 1) . ')');
+$sheet1->setCellValue('I' . $totalRow, '=SUM(I13:I' . ($totalRow - 1) . ')');
+$sheet1->setCellValue('J' . $totalRow, '=SUM(J13:J' . ($totalRow - 1) . ')');
+$sheet1->setCellValue('K' . $totalRow, '=SUM(K13:K' . ($totalRow - 1) . ')');
+$sheet1->setCellValue('L' . $totalRow, '=SUM(L13:L' . ($totalRow - 1) . ')');
+$sheet1->setCellValue('M' . $totalRow, '=SUM(M13:M' . ($totalRow - 1) . ')');
 
 // **Atur Lebar Kolom**
-$sheet->getColumnDimension('A')->setWidth(4.42);
-$sheet->getColumnDimension('B')->setWidth(23.57+0.71);
-$sheet->getColumnDimension('C')->setWidth(19+0.71);
-$sheet->getColumnDimension('D')->setWidth(11.14);
-$sheet->getColumnDimension('E')->setWidth(9.29);
-$sheet->getColumnDimension('F')->setWidth(12+0.71);
-$sheet->getColumnDimension('G')->setWidth(9.14+0.71);
-$sheet->getColumnDimension('H')->setWidth(14.78+0.71);
-$sheet->getColumnDimension('I')->setWidth(8.14+0.71);
-$sheet->getColumnDimension('J')->setWidth(9.29+0.71);
-$sheet->getColumnDimension('K')->setWidth(5.86+0.71);
-$sheet->getColumnDimension('L')->setWidth(5.86+0.71);
-$sheet->getColumnDimension('M')->setWidth(5.86+0.71);
+$sheet1->getColumnDimension('A')->setWidth(4.42);
+$sheet1->getColumnDimension('B')->setWidth(23.57+0.71);
+$sheet1->getColumnDimension('C')->setWidth(19+0.71);
+$sheet1->getColumnDimension('D')->setWidth(11.14);
+$sheet1->getColumnDimension('E')->setWidth(9.29);
+$sheet1->getColumnDimension('F')->setWidth(12+0.71);
+$sheet1->getColumnDimension('G')->setWidth(9.14+0.71);
+$sheet1->getColumnDimension('H')->setWidth(14.78+0.71);
+$sheet1->getColumnDimension('I')->setWidth(8.14+0.71);
+$sheet1->getColumnDimension('J')->setWidth(9.29+0.71);
+$sheet1->getColumnDimension('K')->setWidth(5.86+0.71);
+$sheet1->getColumnDimension('L')->setWidth(5.86+0.71);
+$sheet1->getColumnDimension('M')->setWidth(5.86+0.71);
 
 $headerStyle = [
     'font' => ['bold' => true],
@@ -190,7 +191,7 @@ $headerStyle = [
         'startColor' => ['rgb' => 'D9E1F2']
     ]
 ];
-$sheet->getStyle('A10:M12')->applyFromArray($headerStyle);
+$sheet1->getStyle('A10:M12')->applyFromArray($headerStyle);
 
 $dataStyle = [
     'font' => ['size' => 8],
@@ -230,16 +231,67 @@ $dataStyle3 = [
         ],
     ],
 ];
-$sheet->getStyle('A13:A' . ($rowNum - 1))->applyFromArray($dataStyle);
-$sheet->getStyle('D13:M' . ($rowNum - 1))->applyFromArray($dataStyle);
-$sheet->getStyle('B13:C' . ($rowNum - 1))->applyFromArray($dataStyle2);
-$sheet->getStyle('I' . $totalRow . ':M' . $totalRow)->applyFromArray($dataStyle);
-$sheet->getStyle('A' . $totalRow . ':H' . $totalRow)->applyFromArray($dataStyle3);
+$sheet1->getStyle('A13:A' . ($rowNum - 1))->applyFromArray($dataStyle);
+$sheet1->getStyle('D13:M' . ($rowNum - 1))->applyFromArray($dataStyle);
+$sheet1->getStyle('B13:C' . ($rowNum - 1))->applyFromArray($dataStyle2);
+$sheet1->getStyle('I' . $totalRow . ':M' . $totalRow)->applyFromArray($dataStyle);
+$sheet1->getStyle('A' . $totalRow . ':H' . $totalRow)->applyFromArray($dataStyle3);
 
-// Buat objek writer untuk menyimpan file dalam format XLS
 $writer = new Xls($spreadsheet);
 $nama_lokasi = $rowlokasi['nama_lokasi'] ?? 'Lokasi_Tidak_Diketahui'; 
 $date = date('Y-m-d');
+
+$sheet2 = $spreadsheet->createSheet(1);
+$sheet2->setTitle('KIR QRCode');
+
+// **Header dan Logo**
+$sheet2->setCellValue('C1', 'PEMERINTAH KABUPATEN BREBES');
+$sheet2->setCellValue('C2', 'DINAS KOMUNIKASI INFORMATIKA DAN STATISTIK');
+$sheet2->setCellValue('C3', 'KARTU INVENTARIS RUANGAN');
+$sheet2->mergeCells('C1:K1');
+$sheet2->mergeCells('C2:K2');
+$sheet2->mergeCells('C3:K3');
+$sheet2->mergeCells('A1:B3');
+$sheet2->getStyle('C1:C4')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+$sheet2->getStyle('C1:C4')->getFont()->setBold(true);
+$drawing = new Drawing();
+$drawing->setName('Logo');
+$drawing->setPath('qrcodes/brebes.png');
+$drawing->setWidth(60);
+$drawing->setCoordinates('A1');
+$drawing->setWorksheet($sheet2);
+$drawing2 = new Drawing();
+$drawing2->setName('qrcodes');
+$drawing2->setPath('qrcodes/ruang/inventaris_'.$nama_lokasi.'.png');
+$drawing2->setWidth(420);
+$drawing2->setCoordinates('E13');
+$drawing2->setWorksheet($sheet2);
+
+$sheet2->setCellValue('A5', 'Sub Unit Organisasi');
+$sheet2->setCellValue('A6', 'Pengelola Barang');
+$sheet2->setCellValue('A7', isset($rowlokasi['bid_lokasi']) ? 'Ruangan' : 'Ruangan');
+$sheet2->setCellValue('A8', 'Per Tanggal');
+$sheet2->setCellValue('C5', ': DINAS KOMUNIKASI, INFORMATIKA DAN STATISTIK');
+$sheet2->setCellValue('C6', ': DINAS KOMUNIKASI, INFORMATIKA DAN STATISTIK');
+$sheet2->setCellValue('C7', ': ' . strtoupper($rowlokasi['nama_lokasi']));
+$sheet2->setCellValue('C8', ': ' . date('d F Y'));
+$sheet2->setCellValue('I8', 'No. Kode Lokasi');
+$sheet2->setCellValue('K8', ': ' . $rowlokasi['id_lokasi']);
+
+$sheet2->mergeCells('A5:B5');
+$sheet2->mergeCells('A6:B6');
+$sheet2->mergeCells('A7:B7');
+$sheet2->mergeCells('A8:B8');
+$sheet2->mergeCells('I8:J8');
+$sheet2->mergeCells('K8:M8');
+$sheet2->mergeCells('C5:G5');
+$sheet2->mergeCells('C6:G6');
+$sheet2->mergeCells('C7:G7');
+$sheet2->mergeCells('C8:G8');
+
+$sheet2->getColumnDimension('B')->setWidth(16.86+0.71);
+$sheet2->getColumnDimension('G')->setWidth(18.86+0.71);
+
 // nama file dinamis
 header('Content-Type: application/vnd.ms-excel');
 header('Content-Disposition: attachment; filename="KIR_' . $nama_lokasi . '_' . $date . '.xls"');
---
+++ proses/inventaris/export_label_kir_pdf.php
@@ -0,0 +1,175 @@
+<?php
+ob_start(); // Mulai buffer output
+session_start();
+include('../../koneksi/koneksi.php');
+
+// Ambil data lokasi
+$id_lokasi = $_GET['id_lokasi'];
+$sql_lokasi = "SELECT * FROM lokasi WHERE id_lokasi = '$id_lokasi'";
+$result_lokasi = mysqli_query($conn, $sql_lokasi);
+$rowlokasi = mysqli_fetch_assoc($result_lokasi);
+
+// Ambil data barang
+$sql_barang = "
+    SELECT id_barang_pemda, kode_barang, no_regristrasi, tgl_pembelian, nama_barang, merk 
+    FROM data_barang 
+    WHERE id_ruang_sekarang = '$id_lokasi'
+";
+$result_barang = mysqli_query($conn, $sql_barang);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Label Barang <?php echo $rowlokasi['nama_lokasi']; ?></title>
+    <style>
+        @page {
+            size: A4;
+            margin: 20px;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', serif;
+            /* Ganti menjadi Times New Roman */
+            font-size: 12px;
+            background-color: #f2f2f2;
+        }
+
+
+        .card {
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap;
+            gap: 10px;
+        }
+
+        .column {
+            width: 48%;
+            /* Membuat dua kolom */
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+
+        table {
+            border: 1px solid black;
+            border-collapse: separate;
+            width: 100%;
+            margin-bottom: 10px;
+        }
+
+        th,
+        td {
+            border: 0.3 solid black;
+            padding: 2px;
+            text-align: center;
+        }
+
+        th {
+            background-color: #f0f0f0;
+            font-weight: bold;
+        }
+
+        .qrcode {
+            width: 95px;
+            height: 95px;
+            object-fit: contain;
+        }
+
+        .brebes-img img {
+            width: 40px;
+            height: 40px;
+            object-fit: contain;
+        }
+
+        .kode-barang {
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="card">
+        <?php
+        $count = 0;
+        while ($row = mysqli_fetch_assoc($result_barang)) {
+            $id_barang_pemda = $row['id_barang_pemda'];
+            $kode_barang = $row['kode_barang'];
+            $no_regristrasi = $row['no_regristrasi'];
+            $tgl_pembelian = $row['tgl_pembelian'];
+            $nama_barang = $row['nama_barang'];
+            $merk = $row['merk'];
+        ?>
+            <div class="column">
+                <table>
+                    <colgroup>
+                        <col style="width: 25px;">
+                        <col style="width: 250px;">
+                        <col style="width: 10px;">
+                        <col>
+                    </colgroup>
+                    <tbody>
+                        <tr>
+                            <td rowspan="4" class="brebes-img" style="vertical-align: middle;">
+                                <img src="qrcodes/brebes.png" alt="Logo Brebes">
+                                <br>
+                                <span style="font-size: 10px; margin-top: 5px; display: inline-block;"><?php echo date('Y'); ?></span>
+                            </td>
+                            <td colspan="2" class="kode-barang" style="padding-bottom:3px"><?php echo $id_barang_pemda; ?></td>
+                            <td rowspan="4" style="vertical-align: middle;">
+                                <img src="qrcodes/barang/qrcode_<?php echo $id_barang_pemda; ?>.png" alt="QR Code" class="qrcode">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" class="kode-barang" style="vertical-align: top;"><?php echo $kode_barang; ?></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" style="text-align: left; height:20px; padding-left: 5px"><?php echo $nama_barang; ?></td>
+                        </tr>
+                        <tr>
+                            <td style="text-align: left; font-size: 11.5px; padding-left: 5px"><?php echo $tgl_pembelian; ?></td>
+                            <td style="text-align: right;font-size: 11.5px;"><?php echo $no_regristrasi; ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        <?php
+            $count++;
+        }
+        ?>
+    </div>
+
+</body>
+
+</html>
+
+<?php
+$content = ob_get_clean();
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Spipu\Html2Pdf\Html2Pdf;
+use Spipu\Html2Pdf\Exception\Html2PdfException;
+use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+
+
+$date = date('Y-m-d');
+
+try {
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: attachment; filename="label_barang_'.$rowlokasi['nama_lokasi'].'_'.$date.'.pdf"'); // Attachment untuk otomatis download
+
+    $html2pdf = new Html2Pdf();
+    $html2pdf->pdf->SetDisplayMode('fullpage');
+    $html2pdf->writeHTML($content);
+    
+    // Gunakan 'D' agar file otomatis ter-download
+    $html2pdf->output('label_barang_'.$rowlokasi['nama_lokasi'].'_'.$date.'.pdf', 'D'); 
+} catch (Html2PdfException $e) {
+    echo ExceptionFormatter::getHtmlMessage($e);
+}
+
+?>
